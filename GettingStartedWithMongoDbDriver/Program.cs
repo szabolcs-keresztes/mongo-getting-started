5986d5792d5fc7a33e96789c8a9dc6d774fdad85
Keresztes Szabolcs <Keresztes Szabolcs>
2016-04-26 23:10:11 +0100

    First commit

---
+++ GettingStartedWithMongoDbDriver/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GettingStartedWithMongoDbDriver
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(
+                "This is going to be a project to get started with MongoDb Driver.");
+        }
+    }
+}
